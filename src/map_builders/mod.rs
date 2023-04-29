225ac8acdc71a27ecd432d5fcc87121a8480b464
Rob Wygand <rob.wygand@lookout.com>
2023-04-29 14:53:23 -0700

    Added BSP Interior map generator

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,10 +1,11 @@
 mod simple_map;
 mod common;
 mod bsp_dungeon;
+mod bsp_interior;
 
-use bracket_lib::prelude::*;
 use specs::World;
 use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
+use crate::map_builders::bsp_interior::BspInteriorBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
 use crate::Position;
 use super::Map;
@@ -20,10 +21,11 @@ pub trait MapBuilder {
 
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    let mut rng = RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 1);
-    match builder {
-        1 => Box::new(BspDungeonBuilder::new(new_depth)),
-        _ => Box::new(SimpleMapBuilder::new(new_depth))
-    }
+    // let mut rng = RandomNumberGenerator::new();
+    // let builder = rng.roll_dice(1, 2);
+    // match builder {
+    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
+    //     _ => Box::new(SimpleMapBuilder::new(new_depth))
+    // }
+    Box::new(BspInteriorBuilder::new(new_depth))
 }
