e27bee93c48c071645f76228832774ee66db6c9c
Rob Wygand <rob.wygand@lookout.com>
2023-04-29 15:49:23 -0700

    Added a maze map builder

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -1,21 +1,28 @@
+use bracket_lib::prelude::RandomNumberGenerator;
+use specs::World;
+
 mod simple_map;
 mod common;
 mod bsp_dungeon;
 mod bsp_interior;
 mod cellular_automata;
 mod drunkard;
+mod maze;
 
-use bracket_lib::prelude::RandomNumberGenerator;
-use specs::World;
-use crate::{Position, Map, SHOW_MAPGEN_VISUALIZER};
-use common::*;
-use crate::map_builders::simple_map::*;
-use crate::map_builders::bsp_dungeon::*;
-use crate::map_builders::bsp_interior::*;
-use crate::map_builders::cellular_automata::*;
-use crate::map_builders::drunkard::*;
+use crate::{
+    Position, Map,
+    map_builders::{
+        common::*,
+        simple_map::*,
+        bsp_dungeon::*,
+        bsp_interior::*,
+        cellular_automata::*,
+        drunkard::*,
+        maze::*,
+    }
+};
 
-const MAP_TESTING_MODE:bool  = false;
+const MAP_TESTING_MODE: bool = false;
 
 pub trait MapBuilder {
     fn build_map(&mut self);
@@ -30,19 +37,20 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     match MAP_TESTING_MODE {
         true => {
             // if we're testing, render the last testing map
-            Box::new(DrunkardsWalkBuilder::winding_passages(new_depth))
+            Box::new(MazeBuilder::new(new_depth))
         }
         false => {
             let mut rng = RandomNumberGenerator::new();
-            let builder = rng.roll_dice(1, 7);
-                match builder {
-                    1 => Box::new(BspDungeonBuilder::new(new_depth)),
-                    2 => Box::new(BspInteriorBuilder::new(new_depth)),
-                    3 => Box::new(CellularAutomataBuilder::new(new_depth)),
-                    4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
-                    5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
-                    6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-                    _ => Box::new(SimpleMapBuilder::new(new_depth))
+            let builder = rng.roll_dice(1, 8);
+            match builder {
+                1 => Box::new(BspDungeonBuilder::new(new_depth)),
+                2 => Box::new(BspInteriorBuilder::new(new_depth)),
+                3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+                4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+                5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+                6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
+                7 => Box::new(MazeBuilder::new(new_depth)),
+                _ => Box::new(SimpleMapBuilder::new(new_depth))
             }
         }
     }
