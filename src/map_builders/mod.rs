ba45d70b699602e9cfa5751ea244fb80fc15335c
Rob Wygand <rob.wygand@lookout.com>
2023-04-29 15:15:25 -0700

    Added a cellular automata map builder

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,11 +2,14 @@ mod simple_map;
 mod common;
 mod bsp_dungeon;
 mod bsp_interior;
+mod cellular_automata;
 
+use bracket_lib::prelude::RandomNumberGenerator;
 use specs::World;
 use crate::map_builders::bsp_dungeon::BspDungeonBuilder;
 use crate::map_builders::bsp_interior::BspInteriorBuilder;
 use crate::map_builders::simple_map::SimpleMapBuilder;
+use crate::map_builders::cellular_automata::CellularAutomataBuilder;
 use crate::Position;
 use super::Map;
 
@@ -21,11 +24,12 @@ pub trait MapBuilder {
 
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
-    // let mut rng = RandomNumberGenerator::new();
-    // let builder = rng.roll_dice(1, 2);
-    // match builder {
-    //     1 => Box::new(BspDungeonBuilder::new(new_depth)),
-    //     _ => Box::new(SimpleMapBuilder::new(new_depth))
-    // }
-    Box::new(BspInteriorBuilder::new(new_depth))
+    let mut rng = RandomNumberGenerator::new();
+    let builder = rng.roll_dice(1, 4);
+    match builder {
+        1 => Box::new(BspDungeonBuilder::new(new_depth)),
+        2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        _ => Box::new(SimpleMapBuilder::new(new_depth))
+    }
 }
